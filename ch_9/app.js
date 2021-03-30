6755562031ae4d0e56790de20fdfae8691536692
Asia-Bibi <66402334+Asia-Bibi@users.noreply.github.com>
2021-03-31 01:30:08 +0500

    Add files via upload

---
+++ ch_9/app.js
@@ -0,0 +1,4 @@
+var firstName = prompt("Enter first Name");
+var lastName = prompt("Enter last Name");
+var age = prompt("Enter your age");
+alert(`Hello ${firstName} ${lastName}, you are ${age} years old`);
